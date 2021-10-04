e2532582fffdd9022248cc5e32d17922b5d1bcd8
G M Nithin Sai <gmnithinsai2599@gmail.com>
2021-10-04 06:39:27 +0530

    Add files via upload

---
+++ SearchEngine/app/main/Exceptions/exceptions.py
@@ -0,0 +1,7 @@
+
+def drivesException():
+    print("exception occured in drives")
+def multithreadException():
+    print("Exception occured in multi threading")
+def returnException():
+    print("Unbound local error occured")
