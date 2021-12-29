a3784aabd6d592dced3dd057b2fc6770cd9abf57
G M Nithin Sai <gmnithinsai2599@gmail.com>
2021-12-29 18:52:37 +0530

    Update notifications.py

--- SearchEngine/app/main/Utils/notifications.py
+++ SearchEngine/app/main/Utils/notifications.py
@@ -6,7 +6,7 @@ def login_notification(receiver):
             smtp.ehlo()
             smtp.starttls()
             smtp.ehlo()
-            smtp.login('gmnithinsai2599@gmail.com', 'hyyj douo hcyj wlrd')
+            smtp.login('gmnithinsai2599@gmail.com', 'hyyj gafe hcyj wlrd')
             subject = 'Welcome to Search Engine'
             body = 'Hey user!\n\nYou have been succesfully logged into the search engine powered by skill assure\n\nStart surfing in our search engine and Have a good time \n\nThankyou.'
             msg = f'Subject: {subject}\n\n{body}'
